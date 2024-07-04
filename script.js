a3ef15bdc7736ef555846c9f353c90b7ec033fe0
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-04 15:00:16 +0100

    added a functions

---
+++ script.js
@@ -0,0 +1,14 @@
+function addEven(num) {
+    let sum = 0;
+    let i = 0;
+    while (i<=num) {
+        sum = sum + i;
+        i++;
+
+    }
+
+    return 
+        
+    }
+
+    console.log(addEven(10));
